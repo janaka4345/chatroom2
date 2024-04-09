90228bdba858a24be429f2661f54298c5ad13ff5
banuka <banukaramanayaka76@gmail.com>
2024-04-09 18:24:33 +0530

    other:misc

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -16,5 +16,5 @@ app.get('/', (req, res) => {
 const port = process.env.PORT || 3000;
 
 app.listen(port, () => {
-    console.log(`Server listening on port ${port}`);
+    console.log(`Server listening on port ${port}!`);
   });
