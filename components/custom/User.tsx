ccd9e9d3413313cd3c7e1cce43d591013cc571e0
banuka <banukaramanayaka76@gmail.com>
2024-04-16 22:36:26 +0530

    other:updated the avatar

--- components/custom/User.tsx
+++ components/custom/User.tsx
@@ -1,10 +1,18 @@
+import { auth } from '@/auth'
 import { Avatar, AvatarFallback, AvatarImage } from '@/components/ui/avatar'
 import { cn } from '@/lib/utils'
-export default function User({ className }: { className?: String }) {
+export default async function User({ className }: { className?: String }) {
+    const session = await auth()
+
+    if (!session?.user) {
+        return null
+    }
     return (
         <Avatar className={cn(className)}>
-            <AvatarImage src="https://github.com/shadcn.png" alt="@shadcn" />
-            <AvatarFallback>CN</AvatarFallback>
+            <AvatarImage src={session?.user?.image as string} alt="avatar" />
+            <AvatarFallback>
+                {(session.user?.name as string).slice(0, 2).toUpperCase()}
+            </AvatarFallback>
         </Avatar>
     )
 }
