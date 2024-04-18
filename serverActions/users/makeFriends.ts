4a3e2a25ab13e54189ebe00b9e66e8164aefb9fc
banuka <banukaramanayaka76@gmail.com>
2024-04-18 20:24:31 +0530

    other:friend requests server handle

--- serverActions/users/makeFriends.ts
+++ serverActions/users/makeFriends.ts
@@ -3,10 +3,18 @@ import { auth } from '@/auth'
 import prisma from '@/utils/prismaClient'
 export const makeFriends = async ({ friendId }: { friendId: string }) => {
     const session = await auth()
-    await prisma.user_friend.create({
-        data: {
-            userId: session?.user?.id as string,
-            friendId: friendId,
-        },
+    await prisma.user_friend.createMany({
+        data: [
+            {
+                userId: session?.user?.id as string,
+                friendId: friendId,
+            },
+            {
+                userId: friendId,
+                friendId: session?.user?.id as string,
+            },
+        ],
     })
 }
+
+export const manageFriend = () => {} // TODO manage friend state
