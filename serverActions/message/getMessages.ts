76641dee784a0434ae8566e79860654b95e41c84
banuka <banukaramanayaka76@gmail.com>
2024-04-23 16:39:53 +0530

    feat:display last message on conversation card

--- serverActions/message/getMessages.ts
+++ serverActions/message/getMessages.ts
@@ -1,19 +1,31 @@
 'use server'
+import { auth } from '@/auth'
 import prisma from '@/utils/prismaClient'
 export const getPrivateMessages = async ({
-    senderId,
     receiverId,
 }: {
-    senderId: string
     receiverId: string
 }) => {
+    const session = await auth()
+
     const privateMessages = await prisma.user_message.findMany({
         where: {
             OR: [
-                { AND: [{ senderId: senderId }, { receiverId: receiverId }] },
-                { AND: [{ senderId: receiverId }, { receiverId: senderId }] },
+                {
+                    AND: [
+                        { senderId: session?.user?.id! },
+                        { receiverId: receiverId },
+                    ],
+                },
+                {
+                    AND: [
+                        { senderId: receiverId },
+                        { receiverId: session?.user?.id! },
+                    ],
+                },
             ],
         },
+
         include: {
             message: {
                 select: {
@@ -30,4 +42,44 @@ export const getPrivateMessages = async ({
         },
     })
     return privateMessages
+}
+
+export const getLastMessage = async ({
+    receiverId,
+}: {
+    receiverId: string
+}) => {
+    const session = await auth()
+    const lastMessage = await prisma.user_message.findFirst({
+        orderBy: {
+            message: {
+                sent: 'desc',
+            },
+        },
+        where: {
+            OR: [
+                {
+                    AND: [
+                        { senderId: session?.user?.id! },
+                        { receiverId: receiverId },
+                    ],
+                },
+                {
+                    AND: [
+                        { senderId: receiverId },
+                        { receiverId: session?.user?.id! },
+                    ],
+                },
+            ],
+        },
+        include: {
+            message: {
+                select: {
+                    message: true,
+                    sent: true,
+                },
+            },
+        },
+    })
+    return lastMessage
 }
