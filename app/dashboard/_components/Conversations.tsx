4c108346ecf1c91da6c9398bb4abc2b7c9e1a1f9
banuka <banukaramanayaka76@gmail.com>
2024-04-23 16:43:31 +0530

    style:minor tailwind adjusments

--- app/dashboard/_components/Conversations.tsx
+++ app/dashboard/_components/Conversations.tsx
@@ -5,7 +5,7 @@ export default async function Conversations() {
     const friends = await getFriends() //TODO use ID instead
 
     return (
-        <div className="overflow-y-auto">
+        <div className="overflow-y-auto w-full">
             <h1>Conversations</h1>
             <div className="flex flex-col gap-2 ">
                 {friends.map((friend, i) => (
