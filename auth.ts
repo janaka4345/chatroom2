98a1fed374d1245df04a2c94102f65d5d9048f12
banuka <banukaramanayaka76@gmail.com>
2024-04-25 21:30:49 +0530

    fix: prisma user id error

--- auth.ts
+++ auth.ts
@@ -3,7 +3,7 @@ import NextAuth from 'next-auth'
 import { PrismaAdapter } from '@auth/prisma-adapter'
 import prisma from './utils/prismaClient'
 import authConfig from './auth.config'
-import { setUserStatus } from './serverActions/users/setUser'
+// import { setUserStatus } from './serverActions/users/setUser'
 
 export const {
     handlers: { GET, POST },
@@ -11,27 +11,27 @@ export const {
 } = NextAuth({
     adapter: PrismaAdapter(prisma),
     session: { strategy: 'jwt' },
-    events: {
-        async signIn(message) {
-            if (message.user.id) {
-                await setUserStatus(message.user.id, true)
-            }
+    // events: {
+    //     async signIn(message) {
+    //         // if (message.user.id) {
+    //         //     await setUserStatus(message.user.id, true)
+    //         // }
 
-            // console.log('signin', message)
-        },
-        async signOut(message) {
-            // console.log(message.token.sub)
-            //TODO fix type errors
-            if (message.token.sub) {
-                await setUserStatus(message.token.sub, false)
-            }
-        },
-    },
+    //         // console.log('signin', message)
+    //     },
+    //     async signOut(message) {
+    //         // console.log(message.token.sub)
+    //         //TODO fix type errors
+    //         // if (message.token.sub) {
+    //         //     await setUserStatus(message.token.sub, false)
+    //         // }
+    //     },
+    // },
     callbacks: {
-        signIn(params) {
-            //TODO email verification check
-            return true
-        },
+        //     signIn(params) {
+        //         //TODO email verification check
+        //         return true
+        //     },
 
         session({ session, token }) {
             if (token.sub && session.user) {
