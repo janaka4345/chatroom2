16bb1aeed8c3b5112ff57ed4488edbdfba5c3d29
Janaka dev <janakchamantha12@gmail.com>
2024-05-19 20:43:24 +0530

    feat:friend request toast added

---
+++ components/custom/FriendRequestToast.tsx
@@ -0,0 +1,30 @@
+import { RequestAcceptButton } from "@/app/dashboard/_components/RequestButton"
+import UserAvatar from "./UserAvatar"
+
+export const FriendRequestSentToast = ({ image, name, message, senderId }: { image: string, name: string, message: string, senderId: string }) => {
+    return (
+        <div className="flex flex-col">
+            <h1>{name} has Sent you a friend request</h1>
+            <div className="flex flex-row">
+                <UserAvatar image={image} name={name} />
+                <h1>{name}</h1>
+            </div>
+            <p>{message}</p>
+            <div>
+                <RequestAcceptButton senderId={senderId} />
+            </div>
+        </div>
+    )
+}
+
+export const FriendRequestAcceptedToast = ({ image, name, message, }: { image: string, name: string, message: string, }) => {
+    return (
+        <div className="flex flex-col">
+            <h1>{name} has accepted your friend request</h1>
+            <div className="flex flex-row">
+                <UserAvatar image={image} name={name} />
+                <h1>{name}</h1>
+            </div>
+        </div>
+    )
+}
--- components/ui/sonner.tsx
+++ components/ui/sonner.tsx
@@ -22,6 +22,7 @@ const Toaster = ({ ...props }: ToasterProps) => {
           cancelButton:
             "group-[.toast]:bg-muted group-[.toast]:text-muted-foreground",
         },
+        duration: 3000
       }}
       {...props}
     />
