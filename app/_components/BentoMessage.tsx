4d9b5c7f14c8f9f1a6a5af729204fd35c91d6286
Janaka dev <janakchamantha12@gmail.com>
2024-05-21 22:57:34 +0530

    feat:bento grid added

---
+++ app/_components/BentoMessage.tsx
@@ -0,0 +1,11 @@
+const BentoMessage = () => {
+    return (
+        <div className="relative w-[168px] h-[168px]  flex justify-center items-center  mx-auto overflow-hidden">
+
+            {/* <div className="absolute z-20 h-8 w-[84px] top-1/2 left-0 origin-bottom-right   bg-green-500 animate-spin-slow  " /> */}
+            <div className="absolute z-0 h-8 w-[120px] top-1/2 left-1/2  origin-top-left bg-gradient-to-t  from-blue-500  animate-spin-slow" />
+            <div className="z-10 w-40 h-40 bg-red-500"></div>
+        </div>
+    )
+}
+export default BentoMessage
--- app/page.tsx
+++ app/page.tsx
@@ -1,20 +1,21 @@
-import Link from 'next/link';
 import { auth } from '@/auth';
-import { Button } from '@/components/ui/button';
-
 export default async function Home() {
     const session = await auth();
     return (
-        <div>
-            <h1>Home page</h1>
-            <Button>click</Button>
-            <Link href="/api/auth/signin">Sign In</Link>
-            <br />
+        <section className='grid grid-cols-4 grid-rows-6 gap-4 w-[100dvw] h-[85dvh] px-32 g'>
+            <div className=" bg-red-500 col-start-2 col-end-4 row-start-1 row-end-4" ></div>
+            <div className=" bg-red-500 col-start-4 col-end-5 row-start-1 row-end-3" ></div>
+            <div className=" bg-red-500 col-start-1 col-end-2 row-start-2 row-end-4" ></div>
+            <div className=" bg-red-500 col-start-3 col-end-5 row-start-4 row-end-7" ></div>
+            <div className=" bg-red-500 col-start-1 col-end-3 row-start-4 row-end-6" ></div>
+            <div className=" bg-red-500 col-start-2 col-end-3 row-start-6 row-end-7"></div>
+            <div className=" bg-red-500 col-start-1 col-end-2 row-start-1 row-end-2"></div>
+
+            {/* <h1>Home page</h1>
             <Link href="/api/auth/signout">Sign Out</Link>
-            <br />
-            <Link href="/dashboard">dashboard</Link>
-            <br />
-            <pre>{JSON.stringify(session, null, 2)}</pre>
-        </div>
+            <br /> */}
+
+            {/* <pre>{JSON.stringify(session, null, 2)}</pre> */}
+        </section>
     );
 }
