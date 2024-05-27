dc4d712ec899a6956ec1291e90143ee8b5f0b2c5
Janaka dev <janakchamantha12@gmail.com>
2024-05-27 17:50:18 +0530

    style:style changes to avatar components

--- components/custom/AvatarGroup.tsx
+++ components/custom/AvatarGroup.tsx
@@ -12,7 +12,7 @@ const AvatarGroup = ({
     return (
         <div
             className={cn(
-                ' flex  w-fit origin-left flex-row -space-x-3 transition-all duration-150 hover:-space-x-2',
+                ' flex  w-fit origin-left flex-row -space-x-3 transition-all duration-300 hover:-space-x-2',
                 className
             )}
         >
@@ -21,7 +21,7 @@ const AvatarGroup = ({
                     key={avatar.name}
                     image={avatar.src}
                     name={avatar.name}
-                    className="h-6 w-6 duration-100 hover:h-7 hover:w-7 hover:-translate-y-2 "
+                    className="h-10 w-10 duration-300 hover:h-12 hover:w-12 hover:-translate-y-2 "
                 />
             ))}
         </div>
--- components/ui/avatar.tsx
+++ components/ui/avatar.tsx
@@ -12,7 +12,7 @@ const Avatar = React.forwardRef<
     <AvatarPrimitive.Root
         ref={ref}
         className={cn(
-            'relative flex h-10 w-10 shrink-0 overflow-hidden rounded-full border-2  border-accent',
+            'relative flex h-10 w-10 shrink-0 overflow-hidden rounded-full border-[3px]  border-accent',
             className
         )}
         {...props}
