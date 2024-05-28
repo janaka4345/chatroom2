7af5b823fe5ea89a6c07ffbc69467aaa1fb98615
Janaka dev <janakchamantha12@gmail.com>
2024-05-28 23:18:30 +0530

    style:responsive page design adopted

--- app/page.tsx
+++ app/page.tsx
@@ -12,33 +12,41 @@ export default async function Home() {
 
     const session = await auth();
     return (
-        <section className="grid h-[85dvh] w-[100dvw] grid-cols-4 grid-rows-6 gap-4 px-4  lg:px-20">
-            <div className=" col-start-1 col-end-2 row-start-1 row-end-2 bg-red-500 ">
-                {/* <BentoDescription /> */}1
+        <section className="grid h-[100dvh] md:h-[85dvh] w-[100dvw]  grid-cols-4 md:grid-rows-6 grid-rows-5 gap-2 md:gap-4  px-4  lg:px-20 pb-2">
+            <div className="z-50 col-start-1 col-end-2 row-start-1 row-end-2  md:col-start-1 md:col-end-2 md:row-start-1 md:row-end-2  ">
+
+                <BentoDescription />
+                {/* 1 */}
             </div>
-            <div className="relative  col-start-2 col-end-4 row-start-1 row-end-4  bg-red-500">
-                {/* <BentoImageMove /> */}2
+            <div className="relative col-start-1 col-end-5 row-start-4 row-end-5 md:col-start-2 md:col-end-4 md:row-start-1 md:row-end-4  ">
+                <BentoImageMove />
+                {/* 2 */}
             </div>
-            <div className=" col-start-4 col-end-5 row-start-1 row-end-3 bg-red-500">
-                {/* <BentoMessage /> */}3
+            <div className="col-start-2 col-end-5 row-start-1 row-end-2 md:col-start-4 md:col-end-5 md:row-start-1 md:row-end-3 ">
+                <BentoMessage />
+                {/* 3 */}
             </div>
-            <div className="z-50 col-start-1 col-end-2 row-start-2 row-end-4 bg-red-500">
-                {/* <BentoUsercards /> */}4
+            <div className="z-50 col-start-1 col-end-3 row-start-3 row-end-4  md:col-start-1 md:col-end-2 md:row-start-2 md:row-end-4 ">
+                <BentoUsercards />
+                {/* 4 */}
             </div>
-            <div className="relative col-start-1 col-end-3 row-start-4 row-end-6  bg-red-500">
-                {/* <BentoInfo /> */}5
+            <div className="relative col-start-1 col-end-5 row-start-2 row-end-3 md:col-start-1 md:col-end-3 md:row-start-4 md:row-end-6  ">
+                <BentoInfo />
+                {/* 5 */}
             </div>
             <div
                 style={{
                     background:
                         'radial-gradient(circle at 0px 0px, hsl(var(--primary))  0%, hsl(var(--accent)) 100%)',
                 }}
-                className=" relative col-start-3 col-end-5 row-start-4 row-end-7 overflow-hidden rounded-3xl border border-gray-100   bg-red-500"
+                className=" relative col-start-2 col-end-5 row-start-5 row-end-6 md:col-start-3 md:col-end-5 md:row-start-4 md:row-end-7 overflow-hidden rounded-3xl border border-gray-100   "
             >
-                {/* <BentoPreview /> */}6
+                <BentoPreview />
+                {/* 6 */}
             </div>
-            <div className=" relative col-start-2 col-end-3 row-start-6  row-end-7 bg-red-500">
-                {/* <BentoUserNotification /> */}7
+            <div className=" relative col-start-3 col-end-5 row-start-3 row-end-4 md:col-start-2 md:col-end-3 md:row-start-6  md:row-end-7 ">
+                <BentoUserNotification />
+                {/* 7 */}
             </div>
 
             {/* <h1>Home page</h1>
