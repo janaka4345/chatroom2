0e3d72927536500578840e3ce4207bfde3ed7ef7
Janaka dev <janakchamantha12@gmail.com>
2024-05-29 22:59:16 +0530

    other:user avatar interactions added

--- components/custom/User.tsx
+++ components/custom/User.tsx
@@ -1,5 +1,15 @@
 import { auth } from '@/auth';
+import {
+    DropdownMenu,
+    DropdownMenuContent,
+    DropdownMenuGroup,
+    DropdownMenuItem,
+    DropdownMenuLabel,
+    DropdownMenuSeparator,
+    DropdownMenuTrigger
+} from "@/components/ui/dropdown-menu";
 import AvatarIcon from './AvatarIcon';
+import Link from 'next/link';
 export default async function User({ className }: { className?: string }) {
     const session = await auth();
 
@@ -7,11 +17,39 @@ export default async function User({ className }: { className?: string }) {
         return null;
     }
     return (
-        <AvatarIcon
-            className={className}
-            image={session?.user?.image!}
-            name={session.user?.name as string}
-        />
+        <DropdownMenu>
+            <DropdownMenuTrigger asChild>
+                <AvatarIcon
+                    className={`${className} cursor-pointer`}
+                    image={session?.user?.image!}
+                    name={session.user?.name as string}
+                />
+            </DropdownMenuTrigger>
+            <DropdownMenuContent className="w-56">
+                <DropdownMenuLabel>
+                    <h1>name name2</h1>
+                    <p>email</p>
+                </DropdownMenuLabel>
+                <DropdownMenuSeparator />
+                <DropdownMenuGroup>
+                    <DropdownMenuItem>
+                        <Link href='/dashboard'>Dashboard</Link>
+                    </DropdownMenuItem>
+                    <DropdownMenuItem>
+                        <Link href='/notification'>Notifications</Link>
+                    </DropdownMenuItem>
+                    <DropdownMenuItem>
+                        <Link href='/settings'>Settings</Link>
+                    </DropdownMenuItem>
+                </DropdownMenuGroup>
+                <DropdownMenuSeparator />
+
+                <DropdownMenuSeparator />
+                <DropdownMenuItem>
+                    <Link href='/api/auth/signout'>Log out</Link>
+                </DropdownMenuItem>
+            </DropdownMenuContent>
+        </DropdownMenu>
         // TODO user setiings including edit profile sign out etc
     );
 }
