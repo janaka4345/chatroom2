ad95118f9409ba37a748500dbe9cf56bcb114f98
Janaka dev <janakchamantha12@gmail.com>
2024-07-28 17:58:30 +0530

    other:stats components data updated

--- app/_components/Stats.tsx
+++ app/_components/Stats.tsx
@@ -4,16 +4,16 @@ const Stats = () => {
             <div className="max-w-screen-xl px-4 py-8 mx-auto text-center lg:py-16 lg:px-6">
                 <dl className="grid max-w-screen-md gap-8 mx-auto text-gray-900 sm:grid-cols-3 ">
                     <div className="flex flex-col items-center justify-center">
-                        <dt className="mb-2 text-3xl md:text-4xl font-extrabold">73M+</dt>
-                        <dd className="font-light text-gray-500 ">developers</dd>
+                        <dt className="mb-2 text-3xl md:text-4xl font-extrabold">12M+</dt>
+                        <dd className="font-light text-gray-500 ">users connected</dd>
                     </div>
                     <div className="flex flex-col items-center justify-center">
-                        <dt className="mb-2 text-3xl md:text-4xl font-extrabold">1B+</dt>
-                        <dd className="font-light text-gray-500 ">contributors</dd>
+                        <dt className="mb-2 text-3xl md:text-4xl font-extrabold">250M+</dt>
+                        <dd className="font-light text-gray-500 ">Messages sent and received</dd>
                     </div>
                     <div className="flex flex-col items-center justify-center">
-                        <dt className="mb-2 text-3xl md:text-4xl font-extrabold">4M+</dt>
-                        <dd className="font-light text-gray-500 ">organizations</dd>
+                        <dt className="mb-2 text-3xl md:text-4xl font-extrabold">50+</dt>
+                        <dd className="font-light text-gray-500 ">Trusted by Organizations</dd>
                     </div>
                 </dl>
             </div>
