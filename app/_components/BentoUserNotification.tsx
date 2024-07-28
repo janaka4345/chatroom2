485e8b469148604d89a466f43fb859e36fff53e6
Janaka dev <janakchamantha12@gmail.com>
2024-07-28 17:56:45 +0530

    style: style changes for mobile devices

--- app/_components/BentoUserNotification.tsx
+++ app/_components/BentoUserNotification.tsx
@@ -2,7 +2,7 @@ import AvatarIcon from '@/components/custom/AvatarIcon';
 
 const BentoUserNotification = () => {
     return (
-        <div className="flex rounded-xl  border border-gray-200  bg-gradient-to-l from-accent to-gray-200 p-1 text-gray-500 shadow md:min-h-full md:p-2">
+        <div className="flex rounded-xl  border border-gray-200  bg-gradient-to-l from-accent to-gray-200 p-1 text-gray-500 shadow md:min-h-full lg:p-2">
             <AvatarIcon
                 className="h-7 w-7 md:h-10 md:w-10"
                 image="https://flowbite.s3.amazonaws.com/blocks/marketing-ui/avatars/karen-nelson.png"
@@ -12,7 +12,7 @@ const BentoUserNotification = () => {
                 <span className="mb-1 text-xs font-semibold text-gray-900 md:text-base ">
                     Jese Leos
                 </span>
-                <div className="line-clamp-2 text-xs font-normal md:line-clamp-none md:text-sm">
+                <div className="line-clamp-2 text-xs font-normal lg:line-clamp-none md:text-sm">
                     Hi Neil, thanks for the dinner last night. It was wonderful.
                 </div>
             </div>
