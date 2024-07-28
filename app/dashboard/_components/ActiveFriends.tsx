ad515cb238456ab0937ff178887bd465c4388484
Janaka dev <janakchamantha12@gmail.com>
2024-07-28 18:00:03 +0530

    fix:div is visible when no friend id online fixed

--- app/dashboard/_components/ActiveFriends.tsx
+++ app/dashboard/_components/ActiveFriends.tsx
@@ -5,10 +5,12 @@ import Link from "next/link";
 
 async function ActiveFriends({ className }: { className?: string }) {
     const friends = await getFriends();
-
+    if (!friends || friends.length < 1) {
+        return null
+    }
     return (
         <div className={cn('rounded-xl bg-white border  flex h-fit  flex-row gap-2 py-2 pl-4 w-full overflow-x-auto overflow-hidden', className)}>
-            {friends?.map((friend, i) => (
+            {friends?.map((friend, _) => (
                 friend.friend.status ? <Link key={friend.friend.name} href={`/dashboard/conversations/${friend.friendId}`}>
                     <UserAvatar
                         name={friend.friend.name!}
