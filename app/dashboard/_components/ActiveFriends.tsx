7f067b5776c62a62f6d4121613d5359f2121ad06
Janaka dev <janakchamantha12@gmail.com>
2024-07-29 23:38:13 +0530

    fix: empty online friends  list making empty nodes fixed

--- app/dashboard/_components/ActiveFriends.tsx
+++ app/dashboard/_components/ActiveFriends.tsx
@@ -5,19 +5,25 @@ import Link from "next/link";
 
 async function ActiveFriends({ className }: { className?: string }) {
     const friends = await getFriends();
-    if (!friends || friends.length < 1) {
+    const ActiveFriendsList = friends?.map((friend, _) => (
+        friend.friend.status ? <Link key={friend.friend.name} href={`/dashboard/conversations/${friend.friendId}`}>
+            <UserAvatar
+                name={friend.friend.name!}
+                image={friend.friend.image!}
+            /> </Link> : null
+    ))
+
+
+
+    if (!friends || friends.length < 1 || !ActiveFriendsList) {
+        return null
+    }
+    if (ActiveFriendsList?.every(friend => friend === null)) {
         return null
     }
     return (
         <div className={cn('rounded-xl bg-white border  flex h-fit  flex-row gap-2 py-2 pl-4 w-full overflow-x-auto overflow-hidden', className)}>
-            {friends?.map((friend, _) => (
-                friend.friend.status ? <Link key={friend.friend.name} href={`/dashboard/conversations/${friend.friendId}`}>
-                    <UserAvatar
-                        name={friend.friend.name!}
-                        image={friend.friend.image!}
-                    /> </Link> : null
-            ))
-            }
+            {ActiveFriendsList}
         </div>
 
     )
