bab4e279ebd97a1a022b03e884f51fada44fe214
Janaka dev <janakchamantha12@gmail.com>
2024-08-01 19:16:43 +0530

    feat:change name form component added

--- app/dashboard/settings/_components/NameChangeModal.tsx
+++ app/dashboard/settings/_components/NameChangeModal.tsx
@@ -1,16 +1,45 @@
+"use client"
+
 import { Button } from "@/components/ui/button"
 import {
     Dialog,
     DialogContent,
     DialogDescription,
-    DialogFooter,
     DialogHeader,
     DialogTitle,
-    DialogTrigger,
+    DialogTrigger
 } from "@/components/ui/dialog"
+import {
+    Form,
+    FormControl,
+    FormField,
+    FormItem,
+    FormLabel,
+    FormMessage
+} from "@/components/ui/form"
 import { Input } from "@/components/ui/input"
-import { Label } from "@/components/ui/label"
+import { nameChangeFormSchema } from "@/lib/schema"
+import { updateUserName } from "@/serverActions/users/updateUser"
+import { zodResolver } from "@hookform/resolvers/zod"
+import { useForm } from "react-hook-form"
+import { z } from "zod"
+
+
+
 export default function NameChangeModal() {
+
+    const form = useForm<z.infer<typeof nameChangeFormSchema>>({
+        resolver: zodResolver(nameChangeFormSchema),
+        defaultValues: {
+            username: "",
+        },
+    })
+
+    async function onSubmit(values: z.infer<typeof nameChangeFormSchema>) {
+        const response = await updateUserName(values)
+        console.log(response)
+    }
+
     return (
         <Dialog>
             <DialogTrigger asChild className="absolute bottom-0 right-1/4 cursor-pointer ">
@@ -18,28 +47,29 @@ export default function NameChangeModal() {
             </DialogTrigger>
             <DialogContent className="sm:max-w-[425px]">
                 <DialogHeader>
-                    <DialogTitle>Edit profile</DialogTitle>
+                    <DialogTitle>Edit Your Username</DialogTitle>
                     <DialogDescription>
-                        Make changes to your profile here. Click save when you're done.
+                        Make changes to your Username. Click save when you&apos;re done.
                     </DialogDescription>
                 </DialogHeader>
-                <div className="grid gap-4 py-4">
-                    <div className="grid grid-cols-4 items-center gap-4">
-                        <Label htmlFor="name" className="text-right">
-                            Name
-                        </Label>
-                        <Input id="name" value="Pedro Duarte" className="col-span-3" />
-                    </div>
-                    <div className="grid grid-cols-4 items-center gap-4">
-                        <Label htmlFor="username" className="text-right">
-                            Username
-                        </Label>
-                        <Input id="username" value="@peduarte" className="col-span-3" />
-                    </div>
-                </div>
-                <DialogFooter>
-                    <Button type="submit">Save changes</Button>
-                </DialogFooter>
+                <Form {...form}>
+                    <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+                        <FormField
+                            control={form.control}
+                            name="username"
+                            render={({ field }) => (
+                                <FormItem>
+                                    <FormLabel>Username</FormLabel>
+                                    <FormControl>
+                                        <Input placeholder="Jone Doe" {...field} />
+                                    </FormControl>
+                                    <FormMessage />
+                                </FormItem>
+                            )}
+                        />
+                        <Button type="submit">Submit</Button>
+                    </form>
+                </Form>
             </DialogContent>
         </Dialog>
 
