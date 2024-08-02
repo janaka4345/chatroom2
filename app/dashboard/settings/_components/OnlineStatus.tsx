b4b1c86159b41fb43cd44241c72322b9eca2f999
Janaka dev <janakchamantha12@gmail.com>
2024-08-02 20:08:52 +0530

    feat:user online status change  added

--- app/dashboard/settings/_components/OnlineStatus.tsx
+++ app/dashboard/settings/_components/OnlineStatus.tsx
@@ -3,13 +3,26 @@
 import { Label } from "@/components/ui/label"
 import { Switch } from "@/components/ui/switch"
 import { updateUserOnlineStatus } from "@/serverActions/users/updateUser"
+import { useRouter } from "next/navigation"
 import { useEffect, useState } from "react"
+import { toast } from "sonner"
 export default function OnlineStatus({ status }: { status: boolean }) {
+    const router = useRouter()
+
+
     const [first, setfirst] = useState(status)
     const handleChange = async () => {
         const response = await updateUserOnlineStatus(!first)
         setfirst(prev => !prev)
-        console.log(response);
+        // console.log(response);
+        if (response?.success) {
+            toast.success(response?.success)
+            router.refresh()
+
+        }
+        if (response?.error) {
+            toast.error(response?.error)
+        }
 
     }
 
