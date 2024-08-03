deecaafab083b7ae378c89a87dce68f41fa6a963
Janaka dev <janakchamantha12@gmail.com>
2024-08-03 22:21:44 +0530

    other:layout for settings path

--- app/dashboard/settings/layout.tsx
+++ app/dashboard/settings/layout.tsx
@@ -6,9 +6,9 @@ import { ReactNode } from "react";
 export default function layout({ children }: { children: ReactNode }) {
     return (
         <div className="flex flex-col gap-2 border h-[100dvh] shadow-md rounded-3xl">
-            <div className="flex flex-row gap-2">
+            <div className="flex flex-row gap-2 mt-4 ml-2">
                 <Link className={cn(buttonVariants({ variant: "ghost" }), 'shadow-none')} href='/dashboard/settings/profile'>Profile</Link>
-                <Link className={cn(buttonVariants({ variant: "ghost" }), 'shadow-none')} href='/dashboard/settings/profile'>Notifications</Link>
+                <Link className={cn(buttonVariants({ variant: "ghost" }), 'shadow-none')} href='/dashboard/settings/notifications'>Notifications</Link>
                 <Link className={cn(buttonVariants({ variant: "ghost" }), 'shadow-none')} href='/dashboard/settings/billing'>Billing</Link>
                 <Link className={cn(buttonVariants({ variant: "ghost" }), 'shadow-none')} href='/dashboard/settings/security'>Security</Link>
                 <Link className={cn(buttonVariants({ variant: "ghost" }), 'shadow-none')} href='/dashboard/settings/friends'>Friends</Link>
